d986658d4d46917499a392cfe95330110e5b6fb4
hbaiqiao <1343486201@qq.com>
2024-05-29 16:27:25 +0800

    分布式锁的优化，解决负载均衡多个tomocat线程并发一人多单问题

--- hm-dianping/src/main/java/com/hmdp/service/impl/VoucherOrderServiceImpl.java
+++ hm-dianping/src/main/java/com/hmdp/service/impl/VoucherOrderServiceImpl.java
@@ -8,8 +8,10 @@ import com.hmdp.service.ISeckillVoucherService;
 import com.hmdp.service.IVoucherOrderService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.hmdp.utils.RedisIdWorker;
+import com.hmdp.utils.SimpleRedisLock;
 import com.hmdp.utils.UserHolder;
 import org.springframework.aop.framework.AopContext;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -29,6 +31,8 @@ public class VoucherOrderServiceImpl extends ServiceImpl<VoucherOrderMapper, Vou
 
     @Resource
     private ISeckillVoucherService seckillVoucherService;
+    @Resource
+    private StringRedisTemplate stringRedisTemplate;
 
     @Resource
     private RedisIdWorker redisIdWorker;
@@ -50,11 +54,20 @@ public class VoucherOrderServiceImpl extends ServiceImpl<VoucherOrderMapper, Vou
             return Result.fail("库存不足");
         }
         Long userId = UserHolder.getUser().getId();
-        //7 返回订单id
-        synchronized (userId.toString().intern()) {
+        SimpleRedisLock lock = new SimpleRedisLock("order:" + userId, stringRedisTemplate);
+        //获取锁 分布式锁
+        boolean isLock = lock.tryLock(1200);
+        //判断释放获取锁成功
+        if(!isLock){
+             //获取锁失败返回错误或重试
+              return Result.fail("不允许重复下单");
+        }
+        try {
             //获取代理对象(事务)
             IVoucherOrderService proxy = (IVoucherOrderService) AopContext.currentProxy();
             return proxy.createVoucherOrder(voucherId);
+        } finally {
+             lock.unlock();
         }
     }
 
--- hm-dianping/src/main/java/com/hmdp/utils/SimpleRedisLock.java
+++ hm-dianping/src/main/java/com/hmdp/utils/SimpleRedisLock.java
@@ -1,15 +1,28 @@
 package com.hmdp.utils;
 
+import cn.hutool.core.lang.UUID;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.script.DefaultRedisScript;
+import org.springframework.stereotype.Component;
 
+import java.util.Collections;
 import java.util.concurrent.TimeUnit;
+
 
 public class SimpleRedisLock implements ILock{
 
     private String name;
     private StringRedisTemplate stringRedisTemplate;
     private static final String KEY_PREFIX = "lock:";
+    private static final String ID_PREFIX = UUID.randomUUID().toString(true)+"-";
 
+    private  static final DefaultRedisScript<Long> UNLOCK_SCRIPT;
+    static {
+        UNLOCK_SCRIPT = new DefaultRedisScript<>();
+        UNLOCK_SCRIPT.setLocation(new ClassPathResource("unlock.lua"));
+        UNLOCK_SCRIPT.setResultType(Long.class);
+    }
     public SimpleRedisLock(String name,StringRedisTemplate stringRedisTemplate){
         this.name = name;
         this.stringRedisTemplate = stringRedisTemplate;
@@ -17,16 +30,35 @@ public class SimpleRedisLock implements ILock{
     @Override
     public boolean tryLock(long timeoutSec) {
         //获取线程提示
-        long threadId = Thread.currentThread().getId();
+        String threadId = ID_PREFIX + Thread.currentThread().getId();
         String key = KEY_PREFIX + name;
         //获取锁
         Boolean success =  stringRedisTemplate.opsForValue()
-                .setIfAbsent(key,threadId+"",timeoutSec, TimeUnit.SECONDS);
+                .setIfAbsent(key,threadId,timeoutSec, TimeUnit.SECONDS);
         return Boolean.TRUE.equals(success);
     }
+    //调用Lua脚本
 
-    @Override
-    public void unlock() {
-        stringRedisTemplate.delete(KEY_PREFIX + name);
+    public void unlock(){
+        //调用lua脚本  保证判断和删除的原子性
+        stringRedisTemplate.execute(
+                UNLOCK_SCRIPT, Collections.singletonList(KEY_PREFIX+name),
+                ID_PREFIX +Thread.currentThread().getId()
+        );
     }
+
+
+//    @Override
+//    public void unlock() {
+//        //获取线程标示
+//        String threadId = ID_PREFIX + Thread.currentThread().getId();
+//        //获取锁中的标识
+//        String id = stringRedisTemplate.opsForValue().get(KEY_PREFIX + name);
+//        //判断是否一致
+//        if(threadId.equals(id)){
+//            //释放锁
+//            stringRedisTemplate.delete(KEY_PREFIX + name);
+//        }
+//
+//    }
 }
